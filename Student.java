b9a63ec6285e768e4a16dccd6ab52530610597be
aleeza-najmi <najmi.aleeza@gmail.com>
2025-08-14 18:35:07 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,6 +8,24 @@ public class Student {
         this.lastName = lastName;
         this.studentId = studentId;
     }
-
+public double calculate(double num1, double num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero.");
+                    return 0;
+                }
+            default:
+                System.out.println("Invalid operator: " + operator);
+                return 0;
+        }
   
 }
